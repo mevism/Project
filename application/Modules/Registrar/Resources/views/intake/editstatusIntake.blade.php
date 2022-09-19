7214225348a43848b44c31cd199bf1a98167be08
userdevkei <kitilionesmus27@gmail.com>
2022-09-19 12:58:07 +0300

    registrar intakes

--- application/Modules/Registrar/Resources/views/intake/editstatusIntake.blade.php
+++ application/Modules/Registrar/Resources/views/intake/editstatusIntake.blade.php
@@ -1,18 +1,18 @@
 @extends('registrar::layouts.backend')
 
-@section('content') 
+@section('content')
 <div class="bg-body-light">
         <div class="content content-full">
             <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center">
                 <div class="">
-                    
+
                 </div>
                 <nav class="flex-shrink-0 mt-3 mt-sm-0 ms-sm-3" aria-label="breadcrumb">
                     <ol class="breadcrumb breadcrumb-alt">
                         <li class="breadcrumb-item">
                             <a class="link-fx" href="javascript:void(0)">Intakes</a>
                         </li>
-                        
+
                     </ol>
                 </nav>
             </div>
@@ -32,7 +32,7 @@
                     @csrf
                     @method('PUT')
                     <div class="row">
-                      <div class="col-12">                
+                      <div class="col-12">
                       </div>
                         <div  class="col-12">
                           <input type="hidden" class="form-control form-control-alt" id="intake_name_from" name="intake_name_from" placeholder="Intake From">
@@ -41,11 +41,11 @@
                         <div class="col-12">
                          <select name="status"  class="form-control form-control-alt">
                              <option disabled value="{{ $data->status }}" class="form-select">
-                              @if ($data->status === 0)
+                              @if ($data->status == 0)
                                 pending
-                                @elseif ($data->status === 1)
+                                @elseif ($data->status == 1)
                                 Active
-                                @elseif ($data->status  === 2)
+                                @elseif ($data->status  == 2)
                                 Inactive
                                 @else
                                 Suspended
@@ -58,9 +58,9 @@
                          </select>
                         </div>
                     </div>
-                    
+
                   <div>
-                  
+
                   </div><br>
                     <div class="col-12 text-center p-3">
                       <button type="submit" class="btn btn-alt-success" data-toggle="click-ripple">Submit</button>
