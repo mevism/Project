f7b72024dd43672c44a269490b6b4261a638bcff
userdevkei <kitilionesmus27@gmail.com>
2023-03-21 14:25:04 +0300

    Enter Marks Completed

--- application/Modules/Examination/Entities/ExamMarks.php
+++ application/Modules/Examination/Entities/ExamMarks.php
@@ -10,7 +10,7 @@ class ExamMarks extends Model
     use HasFactory;
 
     protected $fillable = [];
-    
+
     protected static function newFactory()
     {
         return \Modules\Examination\Database\factories\ExamMarksFactory::new();
